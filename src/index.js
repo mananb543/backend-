a273d20a5d51be33be7b0731538d74a40d0cf9f6
mananb543 <bhayanimanan05@gmail.com>
2026-09-07 21:35:48 +0530

    lec 7

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+import DB_NAME from "./constant.js";
+import connectDB from "./db/index.js";
+connectDB();
+
+
+
+
+
+/*
+(async () => {
+    try {
+        await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+
+        app.on("error", (error) => {
+            console.log("ERRR: ", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw error;
+    }
+})();
+*/
