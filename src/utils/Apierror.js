ed3c845ce95a56fe451969465856407bc128007f
mananb543 <bhayanimanan05@gmail.com>
2026-09-08 17:43:08 +0530

    Added utility functions and updated app setup

---
+++ src/utils/Apierror.js
@@ -0,0 +1,22 @@
+class Apierror extends Error {
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        errors = [],
+        statck = ""
+    ){
+        super(message);
+        this.statusCode = statusCode;
+        this.data=null;
+        this.message = message;
+        this.success = false;
+        this.errors = this.errors
+
+        if(statck){
+            this.statck = statck;
+        }else{
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+export default Apierror;
